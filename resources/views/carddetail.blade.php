807192c644e9ad68ca70403f5220dede0ff7a559
Rus <saylerplay@gmail.com>
2021-06-30 12:20:53 +0600

    BreadThumbs Changes

--- resources/views/carddetail.blade.php
+++ resources/views/carddetail.blade.php
@@ -11,7 +11,11 @@
 @endphp
 <section class="url">
   <div class="url__text"><a href="/">Главная</a></div>
-  <div class="url__text"><a href="/subcategory/{{ App\Models\Subcategory::find($product->subcategory_id)->id }}">{{ strip_tags(App\Models\Subcategory::find($product->subcategory_id)->name) }}</a></div>
+  @if($product->subcategory_id != null))
+    <div class="url__text"><a href="/subcategory/{{ App\Models\Subcategory::find($product->subcategory_id)->id }}">{{ strip_tags(App\Models\Subcategory::find($product->subcategory_id)->name) }}</a></div>
+  @elseif($product->category_id != null)
+    <div class="url__text"><a href="/category/{{ App\Models\Category::find($product->category_id)->id }}">{{ strip_tags(App\Models\Category::find($product->category_id)->name) }}</a></div>
+  @endif
   <div class="url__text">{{ $product->name }}</div>
 </section>
 
