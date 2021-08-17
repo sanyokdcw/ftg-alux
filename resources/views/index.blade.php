c8fe7c58e230c5b1d0b589e88aab53bb01a015c8
nurzzzone <nurzzzone@gmail.com>
2021-08-17 12:57:48 +0600

    refactor: add default banner in index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,7 +8,8 @@
 
 
 <section class="system">
-  <div class="system__bg" style="background-image: {{ asset("storage/$banner")  }};"></div>
+  @php($image = is_null($banner)? 'images/main-bg.jpg'? asset("storage/$banner"))
+  <div class="system__bg" style="background-image: {{ $image  }};"></div>
   <div class="system__wrapper swiper-container">
     <div class="swiper-wrapper">
       @foreach ($categories_menu as $category)
