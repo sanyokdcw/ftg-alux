7ae3e516ace0e9e647a14989f6480cad636f2a6e
Rus <saylerplay@gmail.com>
2021-09-08 11:24:37 +0600

    link changes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -15,9 +15,8 @@
 	@php
 if(App\Models\Subcategory::where('category_id', $category->id)->where('available', 1) ->first())
 	$sub_link = '/subcategory/' .  App\Models\Subcategory::where('category_id', $category->id)->first()->id;
-else {
+else 
   $sub_link = '/category/'.$category->id;
-}
 @endphp
 
 <a href="{{ $sub_link }}" style="display:flex; flex-direction: column">
