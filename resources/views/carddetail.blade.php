ac0977c62e8b963d5e8af944b888fc1b721c8329
imqngali <55797666+imqngali@users.noreply.github.com>
2021-09-23 15:20:09 +0600

    bread-crumps

--- resources/views/carddetail.blade.php
+++ resources/views/carddetail.blade.php
@@ -12,6 +12,7 @@
 @endphp
 <section class="url">
   <div class="url__text"><a href="/">{{ __('index.main')}}</a></div>
+  <div class="url__text">каталог</div>
   @if($product->subcategory_id != null)
     <div class="url__text"><a href="/subcategory/{{ App\Models\Subcategory::find($product->subcategory_id)->id }}">{{ strip_tags(App\Models\Subcategory::find($product->subcategory_id)->name) }}</a></div>
   @elseif($product->category_id != null)
