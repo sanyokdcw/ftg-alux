f36c177bf67d578b99c00a8d5fee266147af231a
Rus <saylerplay@gmail.com>
2021-10-11 21:53:00 +0600

    first try banner

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -27,7 +27,7 @@
     }
 </style>
   <section class="system">
-    <div class="system__bg" style="background-image: url({{ is_null($banner)? 'images/main-bg.jpg': asset("storage/$banner") }});"></div>
+    <div class="system__bg" style="background-image: url(/storage/{{  str_replace ( '\\', '/', $main_baner->image) }});"></div>
     <div class="system__wrapper swiper-container">
       <div class="swiper-wrapper">
         @foreach ($categories_menu as $category)
