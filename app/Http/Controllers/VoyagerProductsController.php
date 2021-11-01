a4f3eb332d7adc2a2b9de864039f646293591b02
Rus <saylerplay@gmail.com>
2021-11-01 12:00:51 +0600

    voyager controllers new functions add

--- app/Http/Controllers/VoyagerProductsController.php
+++ app/Http/Controllers/VoyagerProductsController.php
@@ -64,4 +64,40 @@ class VoyagerProductsController extends \TCG\Voyager\Http\Controllers\VoyagerBas
                 ]);
         }
     }
+    public function store(Request $request)
+    {
+        $request->validate([
+            'slug' => 'required|string|unique:products,slug'
+        ]);
+        $slug = $this->getSlug($request);
+
+        $dataType = Voyager::model('DataType')->where('slug', '=', $slug)->first();
+
+        // Check permission
+        $this->authorize('add', app($dataType->model_name));
+
+        // Validate fields with ajax
+        $val = $this->validateBread($request->all(), $dataType->addRows);
+
+        if ($val->fails()) {
+            return response()->json(['errors' => $val->messages()]);
+        }
+
+        if (!$request->has('_validate')) {
+            $data = $this->insertUpdateData($request, $slug, $dataType->addRows, new $dataType->model_name());
+
+            event(new BreadDataAdded($dataType, $data));
+
+            if ($request->ajax()) {
+                return response()->json(['success' => true, 'data' => $data]);
+            }
+
+            return redirect()
+                ->route("voyager.{$dataType->slug}.index")
+                ->with([
+                        'message'    => __('voyager::generic.successfully_added_new')." {$dataType->display_name_singular}",
+                        'alert-type' => 'success',
+                    ]);
+        }
+    }
 }
--- app/Http/Controllers/VoyagerSubcategoriesController.php
+++ app/Http/Controllers/VoyagerSubcategoriesController.php
@@ -65,4 +65,40 @@ class VoyagerSubcategoriesController extends \TCG\Voyager\Http\Controllers\Voyag
                 ]);
         }
     }
+    public function store(Request $request)
+    {
+        $request->validate([
+            'slug' => 'required|string|unique:subcategories,slug'
+        ]);
+        $slug = $this->getSlug($request);
+
+        $dataType = Voyager::model('DataType')->where('slug', '=', $slug)->first();
+
+        // Check permission
+        $this->authorize('add', app($dataType->model_name));
+
+        // Validate fields with ajax
+        $val = $this->validateBread($request->all(), $dataType->addRows);
+
+        if ($val->fails()) {
+            return response()->json(['errors' => $val->messages()]);
+        }
+
+        if (!$request->has('_validate')) {
+            $data = $this->insertUpdateData($request, $slug, $dataType->addRows, new $dataType->model_name());
+
+            event(new BreadDataAdded($dataType, $data));
+
+            if ($request->ajax()) {
+                return response()->json(['success' => true, 'data' => $data]);
+            }
+
+            return redirect()
+                ->route("voyager.{$dataType->slug}.index")
+                ->with([
+                        'message'    => __('voyager::generic.successfully_added_new')." {$dataType->display_name_singular}",
+                        'alert-type' => 'success',
+                    ]);
+        }
+    }
 }
